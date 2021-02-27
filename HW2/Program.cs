491215ec072a880763904d3dc595c7095b323d32
Вадим Кутищев <vki-13@yandex.ru>
2021-02-27 21:39:16 +0300

    123

---
+++ HW2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            x = Convert.ToInt32(Console.ReadLine());
+            y = Convert.ToInt32(Console.ReadLine());
+            if (y > 0)
+            {
+                if (x > 0)
+                {
+                    Console.WriteLine("I");
+                }
+                else
+                {
+                    Console.WriteLine("II");
+                }
+            }
+            else
+            {
+                if (x > 0)
+                {
+                    Console.WriteLine("IV");
+                }
+                else
+                {
+                    Console.WriteLine("III");
+                }
+            }
+        }
+    }
+}
