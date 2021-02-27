b02309b664eaa0f50b403664fbb595c43f3bd6e3
Вадим Кутищев <vki-13@yandex.ru>
2021-02-27 21:25:13 +0300

    Add project files.

---
+++ HW1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                a += b;
+            }
+            else
+            {
+                if (a < b)
+                {
+                    a -= b;
+                }
+                else
+                {
+                    a *= b;
+                }
+            }
+            Console.WriteLine(a);
+        }
+    }
+}
