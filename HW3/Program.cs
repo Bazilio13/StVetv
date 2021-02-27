1f614446d742f878b5279a150780be251415079e
Вадим Кутищев <vki-13@yandex.ru>
2021-02-27 22:02:58 +0300

    HW3

---
+++ HW3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HW3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    if (b > c)
+                    {
+                        Console.WriteLine($"{c} {b} {a}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{b} {c} {a}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{b} {a} {c}");
+                }
+            }
+            else
+            {
+                if (b > c)
+                {
+                    if (a > c)
+                    {
+                        Console.WriteLine($"{c} {a} {b}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{a} {c} {b}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{a} {b} {c}");
+                }
+            }
+        }
+    }
+}
